06530eae25558fef0a76c6a1843283685df4923d
Official1808 <sagarmeena775533@gmail.com>
2026-08-25 23:21:02 +0530

    Fix api/index.js - Use CommonJS instead of ES6 export

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
   res.status(200).json({
     message: "PW SARA Backend API is running! 🚀",
     endpoints: {
@@ -6,4 +6,4 @@ export default function handler(req, res) {
       checkStatus: "/api/check-status?username=student"
     }
   });
-}
+};
